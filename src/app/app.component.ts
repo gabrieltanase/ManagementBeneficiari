1679c1113d15003dbde45cc82d2d2006c987806d
Gabriel Tanase <gabycj88@gmail.com>
2024-10-31 12:44:58 +0200

    Added ConfirmationService, MessageService and toast to app Also added small navigation menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, ConfirmDialogModule, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ManagementBeneficiari';
+  title = 'Gestionare Beneficiari';
 }
