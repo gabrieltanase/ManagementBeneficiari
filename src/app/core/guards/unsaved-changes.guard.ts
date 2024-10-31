882ddd559590ed32dcb3ffdda0a9773b8faf1c21
Gabriel Tanase <gabycj88@gmail.com>
2024-10-31 12:38:35 +0200

    Implemented UnsavedChanges guard

--- src/app/core/guards/unsaved-changes.guard.ts
+++ src/app/core/guards/unsaved-changes.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ConfirmationService } from 'primeng/api';
-import { CanDeactivate } from '@angular/router';
+import { CanDeactivate} from '@angular/router';
 
 export interface UnsavedChanges {
   hasUnsavedChanges: () => boolean;
@@ -17,9 +17,13 @@ export class UnsavedChangesGuard  implements CanDeactivate<UnsavedChanges> {
     if (component.hasUnsavedChanges()) {
       return new Observable<boolean>(observer => {
         this.confirmationService.confirm({
-          message: 'You have unsaved changes. Do you really want to leave?',
-          header: 'Unsaved Changes',
+          message: 'Aveți modificări nesalvate. Sigur doriți să părăsiți pagina?',
+          header: 'Modificări nesalvate',
           icon: 'pi pi-exclamation-triangle',
+          acceptLabel: 'Da',
+          rejectLabel: 'Nu',
+          acceptButtonStyleClass: 'bg-red-600 border-red-700 text-white hover:bg-red-800 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-500',
+          rejectButtonStyleClass: 'bg-green-500 border-green-600 text-white hover:bg-green-600 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400',
           accept: () => observer.next(true),
           reject: () => observer.next(false)
         });
